b760d71f0fcb7f2a9c87c82e181bcde937db380f
yujianjun <xnmdyjj@gmail.com>
2012-04-22 22:27:00 +0800

    Revert "Revert "create project""
    This reverts commit 3dfd96f0cc9adbb8536947285af035f6bf250576.

---
+++ ZhaoJieKou/QuestionHeaderView.h
@@ -0,0 +1,13 @@
+//
+//  QuestionHeaderView.h
+//  ZhaoJieKou
+//
+//  Created by Yu Jianjun on 4/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuestionHeaderView : UIView
+
+@end
---
+++ ZhaoJieKou/TempTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TempTableViewController.h
+//  ZhaoJieKou
+//
+//  Created by Yu Jianjun on 4/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TempTableViewController : UITableViewController
+
+@end
